b368b4a82f216fd785a44e2607214ee2c6161d93
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-02-01 13:55:43 +0100

    added variables to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Mine variabler*/
+const articleOne = document.querySelector('.document__one');
+const articleTwo = document.querySelector('.document__two');
+const articleThree = document.querySelector('.document__three');
+const linkOne = document.querySelector('.primaryNavigation__navigationItem:nth-child(1) a');
+const linkTwo = document.querySelector('.primaryNavigation__navigationItem:nth-child(2) a');
+const linkThree = document.querySelector('.primaryNavigation__navigationItem:nth-child(3) a');
+
+
